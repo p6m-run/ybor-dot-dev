38d753939444dcc4494bf1b9fdd30d304c13c6ab
Jose Pimienta <jose@ybor.ai>
2025-02-10 15:59:47 -0500

    Use env vars

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -2,14 +2,14 @@ export const prerender = false;
 import type { APIRoute } from "astro";
 import { Client } from "@notionhq/client";
 
-const notion = new Client({ auth: 'ntn_e99831864317zxNQ37Z0ZrEcIAkPdYXcvcb46M5llk1gjJ' });
+const notion = new Client({ auth: process.env.NOTION_TOKEN });
 
 export const POST: APIRoute = async ({ request }) => {
     try {
       const data = await request.json();
 
       await notion.blocks.children.append({
-        block_id: '1942530b55ff80bfba01c1324c5fb10f',
+        block_id: process.env.PAGE_ID ?? '',
         children: [
           {
             "object": "block",
