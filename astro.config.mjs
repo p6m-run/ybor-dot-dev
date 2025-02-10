00e61de4990848c8f8d2131e4164b0e46f1a6f48
Jose Pimienta <jose@ybor.ai>
2025-02-10 15:52:42 -0500

    Deploy to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,17 @@ import tailwind from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://redesigned-adventure-8qzrmpk.pages.github.io',
   base: '',
+
   vite: {
     plugins: [tailwind()]
   },
 
-  integrations: [react()]
+  integrations: [react()],
+  adapter: vercel()
 });
